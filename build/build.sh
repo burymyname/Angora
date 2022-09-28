32b247f832d6a6ca4abc8a562cea4bb969b0d943
burymyname <burymyname@163.com>
2022-09-28 12:15:08 +0000

    fix build problems

--- build/build.sh
+++ build/build.sh
@@ -4,13 +4,13 @@ ROOT_DIR=$(dirname $(dirname $BIN_PATH))
 
 set -euxo pipefail
 
-if ! [ -x "$(command -v llvm-config)"  ]; then
-    ${ROOT_DIR}/build/install_llvm.sh
-    export PATH=${HOME}/clang+llvm/bin:$PATH
-    export LD_LIBRARY_PATH=${HOME}/clang+llvm/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
-    export CC=clang
-    export CXX=clang++
-fi
+# if ! [ -x "$(command -v llvm-config)"  ]; then
+#     ${ROOT_DIR}/build/install_llvm.sh
+#     export PATH=${HOME}/clang+llvm/bin:$PATH
+#     export LD_LIBRARY_PATH=${HOME}/clang+llvm/lib${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}
+#     export CC=clang
+#     export CXX=clang++
+# fi
 
 PREFIX=${PREFIX:-${ROOT_DIR}/bin/}
 
